4a6f1810ac2fce3bba18db3d8683cab30b451bd0
mikhail iogansen <mikhio@miMB.local>
2023-04-18 12:25:18 +0300

    Connect bootstrap, add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './components/Header'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    ); 
+  }
 }
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Header.css'
+
+class Header extends React.Component {
+	render() {
+    return (
+      <div className="Header">
+        <Navbar expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">SchRod</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+              >
+                <Nav.Link href="#">Home</Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+	}
+}
+
+export default Header;
